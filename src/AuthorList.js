1dd81a6e292597748d935c2603d120dc064d7f2e
roman <romka69tver@gmail.com>
2020-07-24 03:21:04 +0300

    add toogle slice for AuthorList

--- src/AuthorList.js
+++ src/AuthorList.js
@@ -3,22 +3,44 @@ import React from "react"
 import AuthorCard from "./AuthorCard"
 
 class AuthorList extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = { showAllAuthors: false }
+    this.numOfVisibleAuthors = 3
+  }
+
+  toggle () {
+    this.setState({ showAllAuthors: !this.state.showAllAuthors })
+  }
+
+  sliceAuthors () {
+    if (this.state.showAllAuthors)
+      return this.props.authors
+
+    return this.props.authors.slice(0, this.numOfVisibleAuthors)
+  }
+
   render () {
     if (!this.props.authors)
       return <div>Empty Author</div>
 
     return (
       <div>
+        <div style={styles.action}>
+          <button onClick={() => this.toggle()}>
+            Show all Authors
+          </button>
+        </div>
         {
-          this.props.authors
+          this.sliceAuthors()
             .map(author => (
-              <div key={author.Id} style={styles.item}>
+              <div key={author.Id}>
                 <AuthorCard author={author} />
               </div>
             ))
         }
       </div>
-
     )
   }
 }
@@ -26,5 +48,8 @@ class AuthorList extends React.Component {
 export default AuthorList
 
 const styles = {
-
+  action: {
+    display: "flex",
+    justifyContent: "flex-end",
+  },
 }
