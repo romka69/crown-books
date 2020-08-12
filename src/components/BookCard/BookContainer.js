2383e0aeeeb1edc64f21c4d49d3df71077da7fec
roman <romka69tver@gmail.com>
2020-08-12 20:05:50 +0300

    add HOC component for fetch data

--- src/components/BookCard/BookContainer.js
+++ src/components/BookCard/BookContainer.js
@@ -4,29 +4,15 @@ import BookCard from "./index"
 import RelatedBooks from "../RelatedBooks"
 import AuthorList from "../AuthorList"
 
-import { fetchBookById, fetchAuthors, fetchBooks } from "../../data/fetchData"
+import withBook from "../HOC/withBook"
 
 class BookContainer extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      book: null,
-      authorsData: null,
-      relatedBooks: null,
-    }
-  }
-
-  componentDidMount () {
-    this.getData()
-  }
-
   render () {
-    const { book, authorsData, relatedBooks } = this.state
+    const { bookData: { book, authorsData, relatedBooks } } = this.props
 
     return (
       <>
-        <BookCard isLoading={[!book, "Book card"]} book={book} />
+        <BookCard isLoading={[!book, "Book"]} book={book} />
 
         <RelatedBooks isLoading={[!relatedBooks, "Related books"]} books={relatedBooks} />
 
@@ -34,35 +20,6 @@ class BookContainer extends React.Component {
       </>
     )
   }
-
-  getData () {
-    fetchBookById("rechou1YPRb0takuM")
-      .then(book => {
-        this.setState({
-          book
-        })
-        this.getAuthors(book.Authors)
-        this.getRelatedBooks(book.RelatedBooks)
-      })
-  }
-
-  getAuthors (ids) {
-    fetchAuthors(ids)
-      .then(authorsData => {
-        this.setState({
-          authorsData
-        })
-      })
-  }
-
-  getRelatedBooks (ids) {
-    fetchBooks(ids)
-      .then(relatedBooks => {
-        this.setState({
-          relatedBooks
-        })
-      })
-  }
 }
 
-export default BookContainer
+export default withBook(BookContainer)
