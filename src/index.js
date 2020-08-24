6898731a4f8facef25f867e806547241c3a01c8b
roman <romka69tver@gmail.com>
2020-08-24 17:04:51 +0300

    add component with authprovider

--- src/index.js
+++ src/index.js
@@ -5,18 +5,11 @@ import App from './App'
 
 import "./index.css"
 
-import AuthContext from "../AuthContext"
-
-const currentUser = {
-  email: "777-vasya-777@mail.com",
-  firstName: "Vasya",
-  lastName: "Pupkin",
-  avatarUrl: "https://store.playstation.com/store/api/chihiro/00_09_000/container/RU/ru/999/EP0149-CUSA09988_00-AV00000000000004/1553541605000/image"
-}
+import AuthProvider from "./components/providers/AuthProvider"
 
 ReactDom.render(
-  <AuthContext.Provider value={currentUser}>
+  <AuthProvider>
     <App />
-  </AuthContext.Provider>,
+  </AuthProvider>,
   document.getElementById("root")
 )
