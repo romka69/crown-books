8ca59732ce8d54b7f8d9b9707d1df6c435d43c7f
roman <romka69tver@gmail.com>
2020-09-22 15:10:26 +0300

    fix validation chema

--- src/data/schemas/validateFieldsBook.js
+++ src/data/schemas/validateFieldsBook.js
@@ -1,6 +1,5 @@
 import * as yup from "yup"
 
-const reuiredField = "Required field"
 const positiveNum = "Must be a positive number"
 const supportedFormats = [
   "image/jpg",
@@ -10,25 +9,29 @@ const supportedFormats = [
 ]
 
 export const schema = yup.object().shape({
-  Title: yup.string().required(reuiredField),
+  Title: yup.string().required(),
   Annotation: yup.string().min(20).max(500).required(),
   Pages: yup.number().positive().required().typeError(positiveNum),
-  Language: yup.string().required(reuiredField),
+  Language: yup.string().required(),
   ProgressPercent: yup.number().min(0).max(100).required().typeError(positiveNum),
   Feeds: yup.number().positive().required().typeError(positiveNum),
-  // Cover: yup.mixed()
-  //   .required("A file is required")
-  //   .test(
-  //     "fileSize",
-  //     "File to large",
-  //     value => value && value[0].size <= 1000000
-  //   )
-  //   .test(
-  //     "fileFormat",
-  //     "Unsupported format",
-  //     value => value && supportedFormats.includes(value[0].type)
-  //   ),
-  Authors: yup.string().required(reuiredField),
+  Cover: yup.mixed()
+    .test(
+      "required",
+      "A File is required",
+      value => value && value.length > 0
+    )
+    .test(
+      "fileFormat",
+      "Unsupported format",
+      value => value && value[0] && supportedFormats.includes(value[0].type)
+    )
+    .test(
+      "fileSize",
+      "File to large",
+      value => value && value[0] && value[0].size <= 1000000
+    ),
+  Authors: yup.string().required(),
   MinPrice: yup.number().positive().required().typeError(positiveNum),
   DesiredPrice: yup.number().positive().required().typeError(positiveNum),
   CurrentSum: yup.number().positive().required().typeError(positiveNum),
